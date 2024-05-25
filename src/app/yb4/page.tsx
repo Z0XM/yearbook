3e87f44eab671470a9b0ab702f9bc6440d7ff2a4
z0xm <mukulz2020@gmail.com>
2024-05-26 02:14:42 +0530

    padding

--- src/app/yb4/page.tsx
+++ src/app/yb4/page.tsx
@@ -9,20 +9,20 @@ import { useState } from 'react';
 export default function YB4() {
 	const [columns, setColumns] = useState(1);
 	return (
-		<div className='flex flex-col w-full h-full justify-center items-center gap-4'>
+		<div className='flex flex-col w-full h-full justify-center items-center gap-4 px-4'>
 			<Link href={'/'}>
 				<Image className='mt-8' src={'/YearBook 2.png'} width={986 * 0.5} height={664 * 0.5} alt='YearBook' />
 			</Link>
-			<span className='text-md'>
+			<div className='text-md'>
 				Note: Fill forms for your friends&#39; profile page. You don&#39;t need to fill all forms! Fill for whomever you
 				wish to.
-			</span>
+			</div>
 			<button className='hover:scale-110' onClick={() => setColumns((_) => (_ === 1 ? 2 : _ === 2 ? 4 : 1))}>
 				{columns === 1 && <SquareSplitVertical size={32} />}
 				{columns === 2 && <GridFour size={32} />}
 				{columns === 4 && <GridNine size={32} />}
 			</button>
-			<div className={`grid grid-cols-${columns} gap-4 p-4`}>
+			<div className={`grid grid-cols-${columns} gap-4`}>
 				<Profiles />
 			</div>
 		</div>
